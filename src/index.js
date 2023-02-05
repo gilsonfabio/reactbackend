43a9a94b525e6b3f7dd2f414d0a7eb9c7791344e
Gilson Fabio <gilsonfabio@gmail.com>
2023-02-05 12:05:08 -0300

    Alteração da chamada do CORS

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ require('dotenv/config');
 const app = express();
 
 app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true" );
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
+    res.header("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
     app.use(cors());
     next();
 });
